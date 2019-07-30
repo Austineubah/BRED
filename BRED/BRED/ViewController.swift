7700363e7f8c9f5c5c7b66c267f1efcbba29b8c7
Augustine Ubah <augustine_ubah@yahoo.com>
2019-07-30 19:28:30 -0400

    start 1

--- BRED/BRED/ViewController.swift
+++ BRED/BRED/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textMessageBox: UITextField!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
